abc5d9317f1c7d344b5714eeb795753b361f3b91
Muhammad Hammad <hammadarshad834@gmail.com>
2022-04-07 00:19:15 +0500

    feat: add unit test cases

---
+++ app/bonds/tests/test_utils.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+from django.test import TestCase
+
+from bonds.utils.fetch_latest_exchange_rate import exchange_rate
+
+
+class TestUtils(TestCase):
+
+    def test_exchange_rate(self):
+        """Test exchange rate utils returns a valid float value"""
+        url = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/oportuno/"
+        payload={}
+        headers = {
+            'Bmx-Token': os.environ["BMX_TOKEN"],
+            'Cookie': 'Hex25802680=!7pXbgc2hsEugyPf3AjKmtUwBnOY1od48baUZs+/8cf+WL77tP/pnDiXEBL0h6Rt89aFcdPSNGVABhw==; SRVCOOKIE=!YSnXWhXte9jD62r3AjKmtUwBnOY1oXexuMFVYy3LfLykzJ7pL6ngUv/jwUdckeu8mqr5NI1ozeDI1g==; TS012f422b=01ab44a5a8622214f6d7b924f42eb80a69d82aee3ca2badc64f70ff9995047bce99ec5137059f932a7228aca6a15459b12e22221ecc83c5448f76f3e023f87644dc011fa230069322d2ab5075af5a3bc6e744b6b21'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        rate = exchange_rate()
+
+        self.assertEqual(
+            rate,
+            float(response.json()['bmx']['series'][0]['datos'][0]['dato'])
+        )
--- app/bonds/urls.py
+++ app/bonds/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from bonds import views
 
 
+app_name = "bond"
+
 router = DefaultRouter()
 router.register(r'sell', views.SellBondViewset, basename='sell')
 router.register(r'list', views.ListBondsViewset, basename='list')
